bc24323479ec3fc464da3d495956daee3bef7d5c
AngaPanga <w13w13@yandex.ru>
2023-07-02 22:21:50 +0300

    восьмое дз

---
+++ hw 8/files_wr/pickel_line.py
@@ -0,0 +1,10 @@
+import csv, pickle
+
+def pic_in_csv(f_name):
+    with open(f_name, "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            print(pickle.dumps(row))
+
+if __name__ == '__main__':
+    pic_in_csv('new_csv.csv')
